8fdb66f465285f5df0e217b4bda60aef9026275f
nlsnnn <lagged.06@mail.ru>
2025-05-22 01:30:39 +0300

    feat: add RandomUserAPI dependency and session management to app

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,7 +1,13 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db.helper import db_helper
+from app.services.random_user.random_api import RandomUserAPI
+
+
+def get_random_api(request: Request) -> RandomUserAPI:
+    return request.app.state.random_api
 
 
 DependsSession = Annotated[AsyncSession, Depends(db_helper.session_getter)]
+DependsRandomAPI = Annotated[RandomUserAPI, Depends(get_random_api)]
--- backend/app/create_fastapi_app.py
+++ backend/app/create_fastapi_app.py
@@ -2,16 +2,22 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.services.random_user.random_api import RandomUserAPI
+from app.core.config import settings
 from app.api import register_routers
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await app.state.random_api.init_session()
     yield
+    await app.state.random_api.close_session()
 
 
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
+
+    app.state.random_api = RandomUserAPI(base_url=settings.random_user_api.url)
 
     app.add_middleware(
         CORSMiddleware,
