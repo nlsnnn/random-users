2ea515be65747834e5e02fab54fb838ab97941dd
nlsnnn <lagged.06@mail.ru>
2025-05-22 21:10:27 +0300

    feat: implement UsersException for error handling in user operations and enhance add_users and get_random_user methods with exception handling

--- backend/app/api/users/service.py
+++ backend/app/api/users/service.py
@@ -1,4 +1,6 @@
+from fastapi import status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
 
 from app.crud.users import (
     get_all_users,
@@ -10,6 +12,7 @@ from app.crud.users import (
 from app.core.schemas.user import UserCreate, UserRead, UserCreateRequest
 from app.services.random_user import RandomUserAPI
 from app.core.schemas.pagination import Pagination
+from app.core.exceptions import UsersException
 
 
 class UserService:
@@ -29,18 +32,37 @@ class UserService:
     @staticmethod
     async def get_user_by_id(db: AsyncSession, user_id: int):
         user = await get_user_by_id(db, user_id)
+        if not user:
+            raise UsersException(
+                message=f"Пользователь с ID {user_id} не найден",
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
         return UserRead.model_validate(user)
 
     @staticmethod
     async def add_users(
         random_api: RandomUserAPI, db: AsyncSession, data: UserCreateRequest
     ):
-        items = await random_api.fetch_random_users(data.count)
-        users = [UserCreate.model_validate(user.model_dump()) for user in items]
-        return await add_users(db, users)
+        try:
+            items = await random_api.fetch_random_users(data.count)
+            users = [UserCreate.model_validate(user.model_dump()) for user in items]
+            return await add_users(db, users)
+        except IntegrityError:
+            raise UsersException(
+                message="Уменьшите кол-во загружаемых пользователей или попробуйте позже",
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
 
     @staticmethod
     async def get_random_user(random_api: RandomUserAPI, db: AsyncSession):
-        user = (await random_api.fetch_random_users(1))[0]
-        orm_user = await create_user(db, UserCreate.model_validate(user.model_dump()))
-        return UserRead.model_validate(orm_user)
+        try:
+            user = (await random_api.fetch_random_users(1))[0]
+            orm_user = await create_user(
+                db, UserCreate.model_validate(user.model_dump())
+            )
+            return UserRead.model_validate(orm_user)
+        except IntegrityError:
+            raise UsersException(
+                message="Не получилось добавить пользователя. Попробуйте еще раз",
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
