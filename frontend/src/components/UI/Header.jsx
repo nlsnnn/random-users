ee6114e9e0dc0919a6cd4ae73d626307a256e84d
nlsnnn <lagged.06@mail.ru>
2025-05-22 20:03:22 +0300

    feat: update Header component layout for improved responsiveness and styling

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -2,14 +2,24 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="flex justify-center items-center p-4 border-[#e7edf4] border-b-2">
-      <div>
-        <Link
-          to="/"
-          className="text-xl text-dark font-bold hover:text-blue-500 transition duration-300"
-        >
-          Random Users
-        </Link>
+    <header className="border-[#e7edf4] border-b-2">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <div>
+          <Link
+            to="/"
+            className="text-xl text-dark font-bold hover:text-blue-500 transition duration-300"
+          >
+            Random Users
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/random"
+            className="ml-4 text-sm text-gray-500 hover:text-blue-500 transition duration-300"
+          >
+            Случайный пользователь
+          </Link>
+        </div>
       </div>
     </header>
   );
